a0452e9b2b36b0f07d2a392604f8ea034879f908
Greg Brimble <developer@gregbrimble.com>
2020-02-02 22:25:55 +0000

    Add /rebase GitHub Action

--- packages/client/src/themes/defaultTheme.ts
+++ packages/client/src/themes/defaultTheme.ts
@@ -1,5 +1,4 @@
 import { colors } from './colors'
-import baseStyled, { ThemedStyledInterface } from 'styled-components'
 
 export const theme = {
   colors,
@@ -10,5 +9,3 @@ type ITheme = typeof theme
 declare module 'styled-components/native' {
   interface DefaultTheme extends ITheme {}
 }
-
-// export const styled = baseStyled as ThemedStyledInterface<typeof theme>
